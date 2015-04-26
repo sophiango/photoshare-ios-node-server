8faf6a5aff089cf39f75f730160d0112488525b9
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-04-26 14:20:51 -0700

    search across metadata and photoname

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,43 @@ var router = express.Router();
 var Photo = require('../models/photo');
 
 // search?option=meta&q=something
-router.get('/search', function(req, res) {
-    var option=req.query.option;
+//router.get('/search', function(req, res) {
+//    var option=req.query.option;
+//    var query=req.query.q;
+//    if (option!==null && query!=null){
+//        if(option == "meta"){
+//            console.log("search for " + option + " " + query);
+//            Photo.find({'$or':[{metadata: {'$in':[query]}},{photoName: {$regex: new RegExp('^' + query, 'i')}}]},{public: true},function(err,foundPhoto){
+//                console.log(foundPhoto);
+//                if (err) {
+//                    console.log(err);
+//                    res.status(400).send("Cannot find");
+//                }
+//                else {
+//                    res.status(200).send(foundPhoto);
+//                }})
+//        }
+//        if(option == "name"){
+//            console.log("search for " + option + " " + query);
+//            Photo.find({photoName: {$regex: new RegExp('^' + query, 'i')}},{public: true},function(err,foundPhoto){
+//                console.log(foundPhoto);
+//                if (err) {
+//                    console.log(err);
+//                    res.status(400).send("Cannot find");
+//                }
+//                else {
+//                    res.status(200).send(foundPhoto);
+//                }})
+//        }
+//    }
+//});
+
+
+router.get('/search', function(req, res) { // can search across metadata and photoname at the same time
     var query=req.query.q;
-    if (option!==null && query!=null){
-        if(option == "meta"){
+    if (query!=null){
             console.log("search for " + option + " " + query);
-            Photo.find({metadata: {'$in':[query]}},{public: true},function(err,foundPhoto){
+            Photo.find({'$or':[{metadata: {'$in':[query]}},{photoName: {$regex: new RegExp('^' + query, 'i')}}]},{public: true},function(err,foundPhoto){
                 console.log(foundPhoto);
                 if (err) {
                     console.log(err);
@@ -18,22 +48,8 @@ router.get('/search', function(req, res) {
                 else {
                     res.status(200).send(foundPhoto);
                 }})
-        }
-        if(option == "name"){
-            console.log("search for " + option + " " + query);
-            Photo.find({photoName: {$regex: new RegExp('^' + query, 'i')}},{public: true},function(err,foundPhoto){
-                console.log(foundPhoto);
-                if (err) {
-                    console.log(err);
-                    res.status(400).send("Cannot find");
-                }
-                else {
-                    res.status(200).send(foundPhoto);
-                }})
-        }
     }
 });
-
 
 router.get('/photos',function (req, res) { // public access for all photos that marked as public
     Photo.find({public:true}, function (err, foundPhoto) {
